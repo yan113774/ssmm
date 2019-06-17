080bf71a12a858761b64f07213008bc9ed8d5421
sy <1137742428@qq.com>
2019-06-17 21:17:30 +0800

    1111

---
+++ src/tets/Test.java
@@ -0,0 +1,26 @@
+package tets;
+
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Test {
+	public static void main(String[] args) {
+//		float count = 3;
+//		float countAll =7;
+//		NumberFormat nt = NumberFormat.getPercentInstance(); 
+//		nt.setMinimumFractionDigits(2);
+//		String format = nt.format((count/countAll));
+//		System.out.println(format);
+//		Calendar calendar = Calendar.getInstance();  
+//       calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) - 7);  
+//       Date today = calendar.getTime();  
+//       SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");  
+//       String result = format.format(today);  
+//     System.out.println(result);
+		int [] nums = {7,15,30,183,365};
+		int i = nums[1-1];
+		System.out.println(i);
+	}
+}
